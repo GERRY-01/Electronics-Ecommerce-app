5931a4ccad3b96754b50ad800d90f17a8eaee6c3
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-30 21:27:52 +0300

    made changes in the view for signup

--- electronicsapp/views.py
+++ electronicsapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.models import User
+from .models import Registration
 
 # Create your views here.
 
@@ -6,6 +9,33 @@ def home(request):
     return render(request,'home.html')
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm-password')
+        
+        if password != confirm_password:
+            messages.error(request, 'Passwords do not match.')
+            return render(request, 'Signup.html')
+        
+        if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email already exists.')
+            return render(request, 'Signup.html')
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username already exists.')
+            return render(request, 'Signup.html')
+        
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        
+        registration = Registration(user=user, phone=phone)
+        registration.save()
+        
+        messages.success(request, 'Registration successful. Now login')
+        return render(request, 'Login.html')
     return render(request,'Signup.html')
 
 def login(request):

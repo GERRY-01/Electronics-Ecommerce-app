baab018b392f5f0bd04fb4eae60e5f7755de9bdc
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-30 20:37:26 +0300

    added the function for logging in

--- electronicsapp/views.py
+++ electronicsapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'home.html')
+
+def signup(request):
+    return render(request,'Signup.html')
+
+def login(request):
+    return render(request,'Login.html')

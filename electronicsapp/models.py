fc63c3330e19e3825708e7d07129e7259b0716a0
Gerry-01 <odhiambogerry010@gmail.com>
2025-06-30 21:32:41 +0300

    removed the min_length feature

--- electronicsapp/models.py
+++ electronicsapp/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import User
 
 class Registration(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone = models.CharField(max_length=10,min_length=10)
+    phone = models.CharField(max_length=10)

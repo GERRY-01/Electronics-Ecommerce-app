0cd8196002557fec69c88735520f755b7041699c
Gerry-01 <odhiambogerry010@gmail.com>
2025-07-03 11:58:54 +0300

    added the url for updating and deleting items from cart

--- electronicsapp/urls.py
+++ electronicsapp/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('delete/<int:id>',views.delete,name='delete'),
     path('editproduct/<int:id>',views.editproduct,name='editproduct'),
     path('add_to_cart/<int:product_id>', views.add_to_cart, name='add_to_cart'),
+    path('update_cart/<int:cart_item_id>', views.update_cart, name='update_cart'),
+    path('remove_from_cart/<int:cart_item_id>', views.remove_from_cart, name='remove_from_cart'),
     path('cart',views.cart,name='cart'),
 ]

ccb9a9c5c5647d957a734a58ae2c1bb0f4b20f8e
Gerry-01 <odhiambogerry010@gmail.com>
2025-07-04 07:52:21 +0300

    added the urls for password reset

--- electronicsapp/urls.py
+++ electronicsapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -15,4 +16,10 @@ urlpatterns = [
     path('update_cart/<int:cart_item_id>', views.update_cart, name='update_cart'),
     path('remove_from_cart/<int:cart_item_id>', views.remove_from_cart, name='remove_from_cart'),
     path('cart',views.cart,name='cart'),
+    
+    #auth paths
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]

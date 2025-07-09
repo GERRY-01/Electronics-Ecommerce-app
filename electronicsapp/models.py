a87ae20ac42931e5e4d2aa7325d30c09608d7976
Gerry-01 <odhiambogerry010@gmail.com>
2025-07-09 10:47:22 +0300

    added the time field in the model

--- electronicsapp/models.py
+++ electronicsapp/models.py
@@ -35,3 +35,4 @@ class Contact(models.Model):
     email = models.EmailField()
     subject = models.CharField(max_length=200)
     message = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
